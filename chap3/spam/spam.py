a4ddefb2c02500ec0044410a19ff9470ebd511a9
Matheus Cunha <mathcunha@gmail.com>
2017-08-26 15:30:59 +0000

    modeling a spam classsifier

--- chap3/spam/spam.py
+++ chap3/spam/spam.py
@@ -2,23 +2,28 @@ import pandas as pd
 import numpy as np
 import codecs
 import array
+import random
+import matplotlib
+import matplotlib.pyplot as plt
 
-from sklearn.pipeline import Pipeline
-from sklearn_pandas import DataFrameMapper
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.model_selection import cross_val_predict, StratifiedShuffleSplit
 from sklearn.metrics import precision_recall_curve, precision_score, recall_score
 from sklearn.linear_model import SGDClassifier
 
 from os import listdir
 from nltk.tokenize import word_tokenize
+from nltk.tokenize import TweetTokenizer
 
 columns = ['name','type', 'spam','path', 'tokens','array']
+tknzr = TweetTokenizer(strip_handles=True, reduce_len=True, preserve_case=False)
 
 dictionary = list()
 
 def loadTokens(filePath):
     with codecs.open(filePath, 'r',encoding='utf-8', errors='ignore') as infile:
-        tokens =  word_tokenize(infile.read())
+        tokens =  tknzr.tokenize(infile.read())
         set_tokens = set(tokens)#unique items
         for item in list(set_tokens):#cleaning tokens
             if item.isalpha() == False: 
@@ -36,7 +41,7 @@ def loadEmailData(directory, spam=1):
         email = pd.concat([pd.DataFrame([[f, directory, spam, filePath , loadTokens(filePath) , None]], columns=columns), email])
     return email
 
-data = pd.concat([loadEmailData('easy_ham', False), loadEmailData('easy_ham_2', False), loadEmailData('hard_ham', False), loadEmailData('spam', True), loadEmailData('spam_2', True)])
+data = pd.concat([loadEmailData('easy_ham', 0), loadEmailData('easy_ham_2', 0), loadEmailData('hard_ham', 0), loadEmailData('spam', 1), loadEmailData('spam_2', 0)])
 #data = pd.concat([loadEmailData('xingar', 1), loadEmailData('nomes', 0)])
 data['id'] = range(1, len(data) + 1)
 data.set_index(['id'], inplace=True)
@@ -44,13 +49,24 @@ data.reset_index()
 
 dictionary = list(set(dictionary))
 
+random.seed(42)
+random.shuffle(dictionary)
+
+#bkp_dictionary = list(dictionary)
+#dictionary = bkp_dictionary[73000:]#just for my laptop
+
 def createTokenArray(tokens):
     item = list()
     for word in dictionary:
         item.append(tokens.count(word))
     return item
 
-data['tok_array'] = data['tokens'].apply(createTokenArray)
+for word in dictionary:
+    def countTokens(tokens):
+        return tokens.count(word)
+    data[word] = data['tokens'].apply(countTokens)
+
+#data['tok_array'] = data['tokens'].apply(createTokenArray)
 
 split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
 for train_index, test_index in split.split(data, data["type"]):
@@ -70,19 +86,37 @@ compare_props["Strat. test %error"] = 100 * compare_props["Stratified-test"] / c
 
 compare_props
 
+class DataFrameSelector(BaseEstimator, TransformerMixin):
+    def __init__(self, attribute_names):
+        self.attribute_names = attribute_names
+    def fit(self, X, y=None):
+        return self
+    def transform(self, X):
+        return X[self.attribute_names].values
 
-mapper = DataFrameMapper([
-    ("tok_array", None),
-], df_out = False)
+cat_pipeline = Pipeline([
+        ('selector', DataFrameSelector(dictionary)),
+    ])
 
-pipeline = Pipeline([
-    ("mapper", mapper)
-])
+full_pipeline = FeatureUnion(transformer_list=[
+        ("cat_pipeline", cat_pipeline),
+    ])
 
 labels = strat_train_set["spam"]
-train_prepared = pipeline.fit_transform(strat_train_set)
+train_prepared = full_pipeline.fit_transform(strat_train_set)
 
 sgd_clf = SGDClassifier(random_state=42)
 y_scores = cross_val_predict(sgd_clf, train_prepared, labels, cv=3, method="decision_function")
 
-precisions, recalls, thresholds = precision_recall_curve(labels, y_scores)
+def plot_precision_recall_vs_threshold(precisions, recalls, thresholds):
+    plt.plot(thresholds, precisions[:-1], "b--", label="Precision", linewidth=2)
+    plt.plot(thresholds, recalls[:-1], "g-", label="Recall", linewidth=2)
+    plt.xlabel("Threshold", fontsize=16)
+    plt.legend(loc="upper left", fontsize=16)
+    plt.ylim([0, 1])
+
+plt.figure(figsize=(8, 4))
+plot_precision_recall_vs_threshold(precisions, recalls, thresholds)
+
+plt.show()
+
